c8a5b81d0ac16f13d726ab1da970cb81f970da53
mizuha1230 <m63103284@gmail.com>
2021-04-20 20:08:21 +0900

    Gimeiのlast_nameとfirst_nameの変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     password_confirmation {password}
 
     person = Gimei.name
-    last_name             {person.last.kanji}
-    first_name            {person.first.kanji}
+    last_name             {person.last.kanji or person.last.hiragana or person.last.katakana}
+    first_name            {person.first.kanji or person.first.hiragana or person.first.katakana}
     last_name_kana        {person.last.katakana}
     first_name_kana       {person.first.katakana}
     birthday              {Faker::Date.birthday}
