ba53663aa17eea133a6ea5c7e364243930848212
mizuha1230 <m63103284@gmail.com>
2021-04-22 22:58:04 +0900

    null: falseの追記

--- db/migrate/20210421124707_create_items.rb
+++ db/migrate/20210421124707_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :product_name
-      t.text :description
-      t.integer :category_id
-      t.integer :status_id
-      t.integer :delivery_charge_id
-      t.integer :shipment_source_id
-      t.integer :day_to_ship_id
-      t.integer :price
+      t.string :product_name        , null: false
+      t.text :description           , null: false
+      t.integer :category_id        , null: false
+      t.integer :status_id          , null: false
+      t.integer :delivery_charge_id , null: false
+      t.integer :shipment_source_id , null: false
+      t.integer :day_to_ship_id     , null: false
+      t.integer :price              , null: false
       t.references :user, foreign_key: true              
       t.timestamps
     end
