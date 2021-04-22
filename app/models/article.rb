27049b30574fdaf4c8d98ff57690a1053ff0b54f
mizuha1230 <m63103284@gmail.com>
2021-04-22 22:56:23 +0900

    不要なモデルの削除

--- app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :status
-  belongs_to :delivery_charge
-  belongs_to :shipment_source
-  belongs_to :day_to_ship
-end
