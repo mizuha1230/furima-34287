10d11edd9140d2d9c7029e15d0893c93c21be28b
mizuha1230 <m63103284@gmail.com>
2021-04-23 20:18:29 +0900

    商品名と説明のバリデーションに、最大文字数を設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :day_to_ship
 
   validates :image, presence: true
-  validates :product_name, presence: true
-  validates :description, presence: true
+  validates :product_name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
   with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }, numericality: { in: 300..9999999, message: 'Out of setting range' } do
     validates :price
   end
