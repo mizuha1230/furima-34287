9726c90047bb9fb02f17787fc046dcce7bd7e742
mizuha1230 <m63103284@gmail.com>
2021-04-24 18:08:50 +0900

    同じバリデーションをwith_optionsでまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,15 +8,25 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipment_source
   belongs_to_active_hash :day_to_ship
 
-  validates :image, presence: true
-  validates :product_name, presence: true, length: { maximum: 40 }
-  validates :description, presence: true, length: { maximum: 1000 }
-  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
-  validates :category_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :status_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :shipment_source_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :day_to_ship_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  with_options presence: true do
+    validates :image
+    validates :product_name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :shipment_source_id
+    validates :day_to_ship_id
+  end
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :shipment_source_id
+    validates :day_to_ship_id
+  end
   
 end
