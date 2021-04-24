45a559274fe7ddec36c2163c8a84c0902a66781c
mizuha1230 <m63103284@gmail.com>
2021-04-24 18:53:00 +0900

    console.logの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,12 +2,8 @@ window.addEventListener('load', () => {
     const priceInput = document.getElementById("item-price");
     priceInput.addEventListener("input", () => {
       const inputValue = priceInput.value;
-      console.log(inputValue);
-
       const addTaxDom = document.getElementById("add-tax-price");
       addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-      console.log(addTaxDom);
-
       const profit = document.getElementById("profit");
         const value_result = inputValue * 0.1;
         profit.innerHTML = (Math.floor(inputValue - value_result));
