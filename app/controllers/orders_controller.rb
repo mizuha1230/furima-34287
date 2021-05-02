208b305fba46bcaa74e9dee785dada623cd58b5b
mizuha1230 <m63103284@gmail.com>
2021-05-02 14:09:39 +0900

    Formオブジェクトのインスタンスを生成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAdress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
 end
