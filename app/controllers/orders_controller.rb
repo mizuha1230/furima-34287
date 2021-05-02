3cb86cb8fd7f654db1656cd7f6723d6dbea20664
mizuha1230 <m63103284@gmail.com>
2021-05-02 22:29:51 +0900

    スペルミスの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    @order_address = OrderAdress.new
+    @order_address = OrderAddress.new
   end
 
   def create
@@ -16,6 +16,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+    params.require(order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
