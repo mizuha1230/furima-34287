72cb6e7fe823481f62e75f9fbc7e1213806db1d2
mizuha1230 <m63103284@gmail.com>
2021-05-02 13:27:41 +0900

    データをテーブルに保存する処理のコード

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,4 +11,9 @@ class OrderAddress
     validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: 'Input only number' }
   end
   validates :prefecture, numericality: {other_than: 1, message: 'Select'}
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
