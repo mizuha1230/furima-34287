49a19a487c0a2b3865c52fdd5f86bd55efb64a1e
mizuha1230 <m63103284@gmail.com>
2021-05-02 13:20:04 +0900

    バリデーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,14 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly'}
+    validates :municipality
+    validates :house_number
+    validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: 'Input only number' }
+  end
+  validates :prefecture, numericality: {other_than: 1, message: 'Select'}
 end
