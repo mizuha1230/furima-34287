99877c1c303b84f32f9a4bd2c5119acfb24fd36f
mizuha1230 <m63103284@gmail.com>
2021-05-05 21:30:52 +0900

    電話番号のバリデーションを11桁の数字のみに変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,7 +8,7 @@ class OrderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly'}
     validates :municipality
     validates :house_number
-    validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: 'Input only number' }
+    validates :phone_number, numericality: { with: /^\d{11}$/, message: 'Input only number' }
     validates :prefecture_id, numericality: {other_than: 1, message: 'Select'}
   end
 
